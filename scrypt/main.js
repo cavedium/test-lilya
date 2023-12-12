7befd7f708e1ceb2ace6198b4f9a1300c2a71a72
cavedium <88856217+cavedium@users.noreply.github.com>
2023-12-13 00:17:16 +0200

    add project files

---
+++ scrypt/main.js
@@ -0,0 +1,29 @@
+document.getElementById("ua").onclick = function () {
+	document.getElementById("AboutMe").innerHTML = "Про мене"
+	document.getElementById("NavSkills").innerHTML = "Навички"
+	document.getElementById("Lang").innerHTML = "Мови"
+	document.getElementById("fullname").innerHTML = "Ліля Кривош"
+	document.getElementById("work").innerHTML = "Студентка КПІ"
+	document.getElementById("aboutmetitle").innerHTML = "Про мене"
+	document.getElementById("aboutmetext").innerHTML = "Привіт, мене звати Ліля Кривош."
+	document.getElementById("birthday").innerHTML = "День народження: 31 січня 2005 року"
+	document.getElementById("location").innerHTML = "Місцезнаходження: Київ, Україна"
+	document.getElementById("mail").innerHTML = "Електронна пошта: lillkakryvosh@gmail.com"
+	document.getElementById("phone").innerHTML = "Телефон: + 380(68) 886 58 27"
+	document.getElementById("skillstitle").innerHTML = "Навички"
+	document.getElementById("skillstext1").innerHTML = "Презентація"
+	document.getElementById("skillstext2").innerHTML = "Комунікація"
+	document.getElementById("skillstext3").innerHTML = "Організація"
+	document.getElementById("skillstext4").innerHTML = "Співробітництво"
+	document.getElementById("langtitle").innerHTML = "Мови"
+	document.getElementById("langtext1").innerHTML = "Я знаю такі мови як"
+	document.getElementById("langtext2").innerHTML = "Українська"
+	document.getElementById("langtext3").innerHTML = "Англійська"
+	document.getElementById("langtext4").innerHTML = "Німецька"
+	document.getElementById("footertitle").innerHTML = "Дякуємо за перегляд!"
+	document.getElementById("footertext").innerHTML = "Веб-сторінка іншими мовами"
+}
+
+document.getElementById("eng").onclick = function () {
+	location.reload()
+}
